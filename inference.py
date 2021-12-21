6c7b57df9512028c14209e1790ac33a2939887ea
ryanwu1717 <ryanwu0817@gmail.com>
2021-12-22 03:51:06 +0800

    0
    0

--- inference.py
+++ inference.py
@@ -4,13 +4,15 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from PIL import Image
+from os import walk
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 def inference(imagePath, modelPath):
-    with open("/home/yoyo30169/VRDL dataset/datasets/classes.txt", "r") as f:
-        classes = f.read().split("\n")
+    # with open("/home/yoyo30169/VRDL dataset/datasets/classes.txt", "r") as f:
+    #     classes = f.read().split("\n")
+    classes = ['ALB', 'BET', 'DOL', 'LAG', 'NoF', 'OTHER', 'SHARK', 'YFT']
     inputSize = 448
     data_transforms_test = transform.Compose([
         transform.Resize(inputSize),
@@ -25,21 +27,23 @@ def inference(imagePath, modelPath):
     image_tensor = data_transforms_test(image).float()
     image_tensor = image_tensor.unsqueeze_(0).to(device)
     output = model(image_tensor)
-    score, pred_int = torch.max(output.data, 1)
-    prediction = classes[int(pred_int)]
+    # score, pred_int = torch.max(output.data, 1)
+    # prediction = classes[int(pred_int)]
 
-    print(score, prediction)
+    # print(score, prediction)
 
-    return prediction
+    return output
 
 
-with open('testing_img_order.txt') as f:
-    test_images = [x.strip() for x in f.readlines()]  # all the testing images
-
+# with open('testing_img_order.txt') as f:
+#     test_images = [x.strip() for x in f.readlines()]  # all the testing images
+mypath = "test"
+f = []
+for (dirpath, dirnames, filenames) in walk(mypath):
+    f.extend(filenames)
 submission = []
-for img in test_images:  # image order is important to your result
-    predicted_class = inference('/home/yoyo30169/VRDL dataset/datasets/testing_images/' +
-                                img, '"Efficientnet-b7"_0.998.pkl')  # the predicted category
-    submission.append([img, predicted_class])
+for img in f :  # image order is important to your result
+    predicted_class = inference('TEST/' +img, '"Efficientnet-b7"_0.998.pkl')  # the predicted category
+    submission.append(output)
 
-np.savetxt('answer.txt', submission, fmt='%s')
+submission.to_csv('result/S2.csv', index=False)
