6c2c932b47494afaa0c919c323b1ebaa326d1198
ryanwu1717 <ryanwu0817@gmail.com>
2021-12-22 20:34:53 +0800

    0
    0

---
+++ buildlist.py
@@ -0,0 +1,26 @@
+TXT = 'train.txt'
+f1 = open(TXT,'a')#open file
+import os
+import glob
+from natsort import natsorted
+classes = ['ALB', 'BET', 'DOL', 'LAG', 'NoF', 'OTHER', 'SHARK', 'YFT']
+
+root_dir = 'train'
+# # Get list of all files in a given directory sorted by name
+# list_of_files = sorted( filter( lambda x: os.path.isfile(os.path.join(dir_name, x)),
+#                         os.listdir(dir_name) ) )
+# # Iterate over sorted list of files and print the file paths 
+# # one by one.
+# for file_path in list_of_files:
+#      if file_path.endswith(".jpg"):
+#         f1.write('data/IOC/train/{}'.format(file_path))
+#         f1.write("\n")
+for tmpclass in classes:
+    dir_name = root_dir+'/'+tmpclass
+    list_of_files = sorted( filter( lambda x: os.path.isfile(os.path.join(dir_name, x)),
+            os.listdir(dir_name) ) )
+    for file_path in list_of_files:
+        if file_path.endswith(".jpg"):
+            f1.write('train/'+tmpclass+'/{}'.format(file_path))
+            f1.write("\n")
+f1.close()#close file 
